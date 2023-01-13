c2e11ba40f0aa64136681f7a98191be9d1837ded
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-13 21:01:53 +0100

    gg wp + Bonus

--- classes/Genre.php
+++ classes/Genre.php
@@ -4,19 +4,13 @@ class Genre {
 private $name;
 private $description;
 private $vote;
+private $releaseDate;
 
-    public function __construct($_name, $_description, $_vote) {
-        $this->setTitle($_name);
+    public function __construct($_name, $_description, $_vote, $_releaseDate) {
+        $this->fetchName($_name);
         $this->setDescription($_description);
-        $this->setGenre($_vote);
-    }
-
-    public function setTitle($_genre) {
-        $this->name = $_genre;
-    }
-
-    public function getTitle() {
-        return $this->name;
+        $this->randomVote($_vote);
+        $this->randomReleaseDate($_releaseDate);
     }
 
     public function setDescription($_description) {
@@ -27,12 +21,25 @@ private $vote;
         return $this->description;
     }
 
-    public function setGenre($_vote) {
-        $this->vote = $_vote;
+    public function fetchName() {
+        $array = ["Action"=>1, "Animation"=>2, "Comedy"=>3, "Drama"=>4, "Fantasy"=>5, "Horror"=>6, "Thriller"=>7, "Western"=>8];
+        $name = (array_rand($array,1));
+
+        $this->name = "Genere: " . $name;
     }
-    
-    public function getGenre() {
-        return $this->vote;
+
+    public function randomVote() {
+        $vote = random_int(min:0, max: 5);
+
+        $this->vote = "Voto: " . $vote;
+    }
+
+    public function randomReleaseDate(){
+        $day = random_int(min:1, max: 31);
+        $month = random_int(min:1, max: 12);
+        $year = random_int(min: 1990, max: 2022);
+
+        $this->releaseDate = "Data rilascio: " . $day . "/" . $month . "/" . $year;
     }
 };
 
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 include_once './classes/Movie.php';
 include_once './classes/Genre.php';
 
-$genre_1 = new Genre("Azione", "Descrizione film 1", "Voto: 5");
+$genre_1 = new Genre("", "Descrizione film 1", "", "");
 $movie_1 = new Movie("Titolo film 1", "01:30:00", $genre_1);
 
-$genre_2 = new Genre("Avventura","Descrizione film 2", "Voto: 4");
+$genre_2 = new Genre("", "Descrizione film 2", "", "");
 $movie_2 = new Movie("Titolo film 2", "01:45:00", $genre_2);
 
 var_dump($movie_1, $movie_2);
